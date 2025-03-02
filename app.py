9c0546eb91cc727d4c140dd34e05c362b0019243
Oliver Schöttner <johnturbo82@gmail.com>
2025-03-02 13:09:34 +0100

    first usable version

--- app.py
+++ app.py
@@ -16,27 +16,64 @@ def index():
         return resp
     else:
         user = request.cookies.get('user')
-    
-    if request.method == 'POST':        
-        value = request.form['value']
-        model.insert_entry(value, user)
         
-        resp = make_response(redirect(url_for('index')))
-        resp.set_cookie('submitted', 'true')
-        return resp
-    
-    entries = model.get_entries()
-    return render_template('index.html', entries=entries, submitted=request.cookies.get('submitted'), user=user)
+    surveys = model.get_all_surveys()
+    participant_counts = model.get_participant_count()
+    return render_template('index.html', surveys=surveys, participant_counts=participant_counts, user=user)
 
 @app.route('/survey', methods=['GET'])
 def survey():
+    user = request.cookies.get('user')
+    if not user:
+        return redirect(url_for('index'))
+
     survey_uuid = request.args.get('uuid')
     if survey_uuid:
         survey = model.get_survey_by_uuid(survey_uuid)
         if survey:
             question_ids = survey['questions'].split(',')
             questions = model.get_questions_by_ids(question_ids)
-            return render_template('survey.html', questions=questions)
+            return render_template('survey.html', questions=questions, survey_uuid=survey_uuid, user=user)
+    return "Survey not found", 404
+
+@app.route('/submit_survey', methods=['POST'])
+def submit_survey():
+    user = request.cookies.get('user')
+    if not user:
+        return redirect(url_for('index'))
+    
+    survey_uuid = request.form.get('survey_uuid')
+    survey = model.get_survey_by_uuid(survey_uuid)
+    if not survey:
+        return "Survey not found", 404
+    
+    question_ids = survey['questions'].split(',')
+    for question_id in question_ids:
+        answer = request.form.get(f'question_{question_id}')
+        if answer:
+            model.insert_answer(question_id, survey_uuid, answer, user)
+    
+    return redirect(url_for('index'))
+
+@app.route('/create_survey', methods=['POST'])
+def create_survey():
+    questions = model.get_random_questions(6)
+    question_ids = ','.join(str(q['id']) for q in questions)
+    survey_uuid = str(uuid.uuid4())
+    model.insert_survey(survey_uuid, question_ids)
+    return redirect(url_for('index'))
+
+@app.route('/results', methods=['GET'])
+def results():
+    survey_uuid = request.args.get('uuid')
+    if survey_uuid:
+        survey = model.get_survey_by_uuid(survey_uuid)
+        if survey:
+            question_ids = survey['questions'].split(',')
+            questions = model.get_questions_by_ids(question_ids)
+            weighted_answers = model.get_weighted_answers_by_survey_uuid(survey_uuid)
+            average_answers = model.get_aggregated_answers_by_survey_uuid(survey_uuid)
+            return render_template('results.html', questions=questions, weighted_answers=weighted_answers, average_answers=average_answers, survey_uuid=survey_uuid)
     return "Survey not found", 404
 
 if __name__ == '__main__':
